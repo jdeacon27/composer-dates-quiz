8878b46a0b4a691312bbc0e23152434d7af5bded
jdeacon27 <jdeacon27@users.noreply.github.com>
2018-08-29 20:12:08 +0100

    Random retrieval and quiz loop

--- src/net/johndeacon/ComposerDatabase.java
+++ src/net/johndeacon/ComposerDatabase.java
@@ -3,6 +3,8 @@ package net.johndeacon;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
 
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
@@ -10,25 +12,48 @@ import com.opencsv.CSVReaderBuilder;
 public class ComposerDatabase {
 	public ComposerDatabase() {
 		try {
+			String[] nextRecord;
 			CSVReader reader = new CSVReaderBuilder(new FileReader("composers.csv")).withSkipLines(1).build();
-			database = reader.readAll();
-//			System.out.println(database);
+			while ( (nextRecord = reader.readNext()) != null ) {
+//				for (String cell : nextRecord) {
+//					System.out.print(cell + "\t");
+//				}
+//				System.out.println();
+				allComposers.add(nextRecord);
+//				System.out.println("'" + nextRecord[knownComposerField] + "'");
+				if ( nextRecord[knownComposerField].length() != 0 ) {
+					knownComposerIndices.add(allComposers.size()-1);	// size is 1 greater than the index of the last element
+				}
+				numberOfKnownComposers = knownComposerIndices.size();
+			}
 			reader.close();
 		} catch(IOException e) {
 			System.out.println("File not found");
 		}
 	}
 	public void test() {
-		String[] testDatabaseEntry = database.get(9);
+		String[] testDatabaseEntry = allComposers.get(0);
+		
+//		System.out.println("Known composer indices List has " + knownComposerIndices.size() + " elements");
+		for ( Integer element : knownComposerIndices ) {
+			System.out.println(allComposers.get(element)[1]);
+		}
 		System.out.println(testDatabaseEntry.length);
 		System.out.println(testDatabaseEntry[1]);
 		System.out.println(testDatabaseEntry[2] + "-" + testDatabaseEntry[3]);
 		System.out.println(testDatabaseEntry[5]);
-		System.out.println("Dummy");
 	}
 	public Composer randomComposer() {
 		return new Composer();
 	}
-	private List<String[]> database;
-
+	
+	public String[] randomKnownComposer() {		// should return a composer
+		return allComposers.get(knownComposerIndices.get(rng.nextInt(numberOfKnownComposers)));
+	}
+	
+	private List<String[]> allComposers= new ArrayList<>();		// should have the read of the CSV file construct Composers and store them
+	private List<Integer> knownComposerIndices = new ArrayList<>();
+	private int knownComposerField = 5;		// sixth element in the String[] array
+	private int numberOfKnownComposers;
+	private Random rng = new Random();
 }
--- src/net/johndeacon/ComposerDatesQuiz.java
+++ src/net/johndeacon/ComposerDatesQuiz.java
@@ -1,12 +1,19 @@
 package net.johndeacon;
 
+import java.util.Scanner;
+
 public class ComposerDatesQuiz {
 	public static void main(String[] args) {
-		 ComposerDatabase composerDatabase = new ComposerDatabase();
-		 composerDatabase.test();
-		 Composer randomComposer = composerDatabase.randomComposer();
-		 System.out.print(randomComposer.forename());
-		 System.out.print(" ");
-		 System.out.println(randomComposer.familyname());
+		ComposerDatabase composerDatabase = new ComposerDatabase();
+//		composerDatabase.test();
+		Scanner sc = new Scanner(System.in);
+		String response;
+		String[] randomKnownComposer;
+		do {
+			randomKnownComposer = composerDatabase.randomKnownComposer();
+			System.out.println(randomKnownComposer[1]);
+			response = sc.next();
+			System.out.println(randomKnownComposer[2] + "-" + randomKnownComposer[3] + "\n");
+		} while ( !response.equalsIgnoreCase("stop") );
 	}
 }
